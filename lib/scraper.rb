7f560bf5f35e07b6c01c4a4545bd2daaa5de22b7
Justin Berger Howes <howes.j.j@gmail.com>
2021-09-15 08:29:16 +1000

    complete

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+puts doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses.each do |course|
+    puts course.text.strip
+  end
+
+  p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
+  
